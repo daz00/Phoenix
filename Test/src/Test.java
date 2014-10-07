ffbf00875b86c6f5933826ad6beb050acf86c999
Taavi <taavikunto@gmail.com>
2014-10-07 19:31:35 +0300

    .

--- Test/src/Test.java
+++ Test/src/Test.java
@@ -3,7 +3,8 @@ public class Test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		//Tahaks teada kas see peabki githubis selline olema
+		//või mitte :D
 	}
 
 }
