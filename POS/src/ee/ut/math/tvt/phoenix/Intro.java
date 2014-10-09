05256ce4af57c3a69646a4c5fd36483a51da3a34
Raina <daz00@hot.ee>
2014-10-09 11:17:03 +0300

    Uus POS

---
+++ POS/src/ee/ut/math/tvt/phoenix/Intro.java
@@ -0,0 +1,10 @@
+package ee.ut.math.tvt.phoenix;
+
+public class Intro {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ POS/src/ee/ut/math/tvt/phoenix/IntroUI.java
@@ -0,0 +1,10 @@
+package ee.ut.math.tvt.phoenix;
+
+public class IntroUI {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
