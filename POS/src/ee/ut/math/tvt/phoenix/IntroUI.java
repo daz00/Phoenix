b7b59a207e60357c0d8dd6df8dfb9667a0c24ad1
juliakaas <kaasjulia@gmail.com>
2014-10-14 22:52:46 +0300

    esialgne introUI, intro peaks olema korras (error parandatud)

--- POS/src/ee/ut/math/tvt/phoenix/IntroUI.java
+++ POS/src/ee/ut/math/tvt/phoenix/IntroUI.java
@@ -1,6 +1,12 @@
 package ee.ut.math.tvt.phoenix;
 
-public class IntroUI {
+import javax.swing.JFrame;
+
+// graafiline kasutajaliides 
+
+public class IntroUI extends JFrame {
+
+	private static final long serialVersionUID = 1L;
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
