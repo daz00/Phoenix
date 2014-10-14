c4947935ba22a122e551f306a01206f0dfbf993a
juliakaas <kaasjulia@gmail.com>
2014-10-14 22:43:58 +0300

    tegin mingi esialgse intro faili

--- POS/src/ee/ut/math/tvt/phoenix/Intro.java
+++ POS/src/ee/ut/math/tvt/phoenix/Intro.java
@@ -1,10 +1,19 @@
 package ee.ut.math.tvt.phoenix;
 
+import org.apache.log4j.Logger;
+import ee.ut.math.tvt.phoenix.IntroUI;
+
 public class Intro {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	private static final Logger log = Logger.getLogger(Intro.class);
+	
+	public static void main(String args[]) {
 
+		IntroUI ui = new IntroUI();
+		ui.setVisible(true);
+
+	log.info("Intro started");
+	
 	}
-
+	
 }
