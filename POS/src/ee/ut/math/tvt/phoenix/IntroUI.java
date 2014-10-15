2a709f2eff919b98370e51fd56b96a248b543f64
Henry Maalinn <ValdekToru@users.noreply.github.com>
2014-10-15 19:58:34 +0300

    Timestamp added

--- POS/src/ee/ut/math/tvt/phoenix/IntroUI.java
+++ POS/src/ee/ut/math/tvt/phoenix/IntroUI.java
@@ -2,23 +2,33 @@ package ee.ut.math.tvt.phoenix;
 
 //import com.jgoodies.looks.windows.WindowsLookAndFeel;
 
+import javafx.scene.shape.Box;
+
 import javax.imageio.ImageIO;
 import javax.swing.JFrame;
 
 import java.awt.BorderLayout;
+import java.awt.Component;
 import java.awt.Container;
 import java.awt.Dimension;
+import java.awt.FlowLayout;
 import java.awt.image.BufferedImage;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.URL;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Properties;
 
 import javax.swing.BoxLayout;
 import javax.swing.ImageIcon;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.LayoutStyle;
+
+
 
 
 // GUI 
@@ -47,6 +57,9 @@ public class IntroUI extends JFrame {
 			prop.load(input);
 			prop2.load(input2);
 
+			DateFormat dateFormat = new SimpleDateFormat("HH:mm dd/MM/yyyy");
+			Calendar cal = Calendar.getInstance();
+			System.out.println(dateFormat.format(cal.getTime())); //2014/08/06 16:00:22
 
 			// get the .properties values 
 
@@ -58,27 +71,36 @@ public class IntroUI extends JFrame {
 					+ prop2.getProperty("build.major.number") + "."
 					+ prop2.getProperty("build.revision.number") + "."
 					+ prop2.getProperty("build.minor.number"));
+			JLabel time = new JLabel(dateFormat.format(cal.getTime()));
 			
 			// Open the link where the logo is (We should probably move it to lib at some point)
 			URL logo_url = new URL(prop.getProperty("teamLogo"));
 			BufferedImage image = ImageIO.read(logo_url);
 			JLabel logo = new JLabel(new ImageIcon(image));
-
-
-			JPanel window = new JPanel();
-			window.setLayout(new BoxLayout(window, BoxLayout.PAGE_AXIS));
-			window.add(logo);
-			window.add(name);
-			window.add(leader);
-			window.add(email);
-			window.add(members);
-			window.add(version);
-
-			Container contentPane = getContentPane();
-			contentPane.add(window, BorderLayout.CENTER);
-			contentPane.setSize(new Dimension(200,200));
 			
-			add(window);
+			
+			
+
+			JPanel panel = new JPanel();
+			panel.setLayout(new BoxLayout(panel, BoxLayout.PAGE_AXIS));
+			panel.add(logo);
+			panel.add(name);
+			panel.add(leader);
+			panel.add(email);
+			panel.add(members);
+			panel.add(version);
+			panel.add(time);
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			add(panel);
 			pack();
 			
 
