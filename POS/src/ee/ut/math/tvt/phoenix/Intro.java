2cc5e3013f937c70709342e2da7577a5456e0e8c
Raina <daz00@hot.ee>
2014-10-23 12:17:19 +0300

    Intro v2, build issues

--- POS/src/ee/ut/math/tvt/phoenix/Intro.java
+++ POS/src/ee/ut/math/tvt/phoenix/Intro.java
@@ -2,21 +2,41 @@ package ee.ut.math.tvt.phoenix;
 
 import org.apache.log4j.Logger;
 
-import ee.ut.math.tvt.phoenix.IntroUI;
+import ee.ut.math.tvt.salessystem.domain.controller.SalesDomainController;
+import ee.ut.math.tvt.salessystem.domain.controller.impl.SalesDomainControllerImpl;
+import ee.ut.math.tvt.salessystem.ui.ConsoleUI;
+import ee.ut.math.tvt.salessystem.ui.SalesSystemUI;
 
 public class Intro {
 
 	private static final Logger log = Logger.getLogger(Intro.class);
-	
-	public static void main(String args[]) {
+	private static final String MODE = "console";
 
-		IntroUI ui = new IntroUI();
-		ui.setVisible(true);
+	public static void main(String[] args) {
 
-	log.info("Intro started");
-	
+		final SalesDomainController domainController = new SalesDomainControllerImpl();
+
+		if (args.length == 1 && args[0].equals(MODE)) {
+			log.debug("Mode: " + MODE);
+
+			ConsoleUI cui = new ConsoleUI(domainController);
+			cui.run();
+		} else {
+
+			IntroUI introUI = new IntroUI();
+			introUI.setVisible(true);
+			introUI.setAlwaysOnTop(true);
+
+			final SalesSystemUI ui = new SalesSystemUI(domainController);
+			ui.setVisible(true);
+
+			introUI.setAlwaysOnTop(false);
+			try {
+				Thread.sleep(3000);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+			introUI.setVisible(false);
+		}
 	}
-
-
-	
 }
