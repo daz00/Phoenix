0afb7e70a806bc58ad2bfd828b4ae3454e62da1f
Taavi <taavikunto@gmail.com>
2014-10-28 23:08:43 +0200

    incomplete Warehouse

--- POS/src/ee/ut/math/tvt/salessystem/ui/tabs/StockTab.java
+++ POS/src/ee/ut/math/tvt/salessystem/ui/tabs/StockTab.java
@@ -1,15 +1,28 @@
 package ee.ut.math.tvt.salessystem.ui.tabs;
 
+import ee.ut.math.tvt.salessystem.domain.data.StockItem;
 import ee.ut.math.tvt.salessystem.ui.model.SalesSystemModel;
+
 import java.awt.Color;
 import java.awt.Component;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.FocusEvent;
+import java.awt.event.FocusListener;
+import java.util.List;
+
 import javax.swing.BorderFactory;
 import javax.swing.JButton;
+import javax.swing.JComboBox;
+import javax.swing.JComponent;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
+import javax.swing.JTextField;
 import javax.swing.table.JTableHeader;
 
 
@@ -45,18 +58,74 @@ public class StockTab {
     panel.add(drawStockMainPane(), gc);
     return panel;
   }
+  // Formatting constraints for the dialogPane
+  private GridBagConstraints getDialogPaneConstraints() {
+      GridBagConstraints gc = new GridBagConstraints();
+
+      gc.anchor = GridBagConstraints.NORTH;
+      gc.weightx = 0.2;
+      gc.weighty = 0d;
+      gc.gridwidth = GridBagConstraints.REMAINDER;
+      gc.fill = GridBagConstraints.NONE;
+
+      return gc;
+  }
 
   // warehouse menu
   private Component drawStockMenuPane() {
     JPanel panel = new JPanel();
-
     GridBagConstraints gc = new GridBagConstraints();
+
+    panel.setBorder(BorderFactory.createTitledBorder("Product"));
+    
+
     GridBagLayout gb = new GridBagLayout();
+    
 
     panel.setLayout(gb);
+    panel.setLayout(new GridLayout(6, 2));
+    // Initialize the textfields
+    final JTextField barCodeField = new JTextField();
+    final JTextField quantityField = new JTextField();
+    final JTextField priceField = new JTextField();
+    final JTextField nameField = new JTextField();
+    final JTextField description = new JTextField();
+
+    
+    // - bar code
+    panel.add(new JLabel("Bar code(id):"));
+    panel.add(barCodeField);
+    
+    panel.add(new JLabel("Name:"));
+    panel.add(nameField);
+    
+    panel.add(new JLabel("Description:"));
+    panel.add(description);
+    
+    // - price
+    panel.add(new JLabel("Price:"));
+    panel.add(priceField);
+
+    // - amount
+    panel.add(new JLabel("Amount:"));
+    panel.add(quantityField);  
 
     gc.anchor = GridBagConstraints.NORTHWEST;
     gc.weightx = 0;
+    
+    //TODO Järgnev kood tuleks kuidagi ümber muuta või õigesse kohta vahele lükata
+    /*addItem.addActionListener(new ActionListener() {
+   	 
+        public void actionPerformed(ActionEvent e)
+        {
+            StockItem addedItem = new StockItem(Long.parseLong(barCodeField.getText()),
+            		nameField.getText(),
+            		description.getText(),
+            		Double.parseDouble(quantityField.getText()),
+            		Integer.getInteger(priceField.getText()));
+            System.out.println(addedItem.getName());
+        }
+    });*/
 
     addItem = new JButton("Add");
     gc.gridwidth = GridBagConstraints.RELATIVE;
