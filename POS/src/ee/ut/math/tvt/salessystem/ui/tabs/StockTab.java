bf44c8d1eeae26b414783ea47a4db692959007f3
Taavi <taavikunto@gmail.com>
2014-10-29 21:33:21 +0200

    Add to warehouse

--- POS/src/ee/ut/math/tvt/salessystem/ui/tabs/StockTab.java
+++ POS/src/ee/ut/math/tvt/salessystem/ui/tabs/StockTab.java
@@ -2,6 +2,7 @@ package ee.ut.math.tvt.salessystem.ui.tabs;
 
 import ee.ut.math.tvt.salessystem.domain.data.StockItem;
 import ee.ut.math.tvt.salessystem.ui.model.SalesSystemModel;
+import ee.ut.math.tvt.salessystem.ui.model.StockTableModel;
 
 import java.awt.Color;
 import java.awt.Component;
@@ -23,10 +24,18 @@ import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
 import javax.swing.JTextField;
+import javax.swing.table.AbstractTableModel;
 import javax.swing.table.JTableHeader;
 
 
-public class StockTab {
+public class StockTab extends StockTableModel {
+
+	private static final long serialVersionUID = 1L;
+	JTextField barCodeField;
+    JTextField quantityField;
+    JTextField priceField;
+    JTextField nameField;
+    JTextField description;
 
   private JButton addItem;
 
@@ -72,6 +81,18 @@ public class StockTab {
   }
 
   // warehouse menu
+  
+  private JButton createAddButton() {
+	    JButton addItem = new JButton("Add");
+	    addItem.addActionListener(new ActionListener() {
+	      public void actionPerformed(ActionEvent e) {
+	       addNewItem();
+	      }
+	    });
+
+	    return addItem;
+	  }
+
   private Component drawStockMenuPane() {
     JPanel panel = new JPanel();
     GridBagConstraints gc = new GridBagConstraints();
@@ -85,11 +106,11 @@ public class StockTab {
     panel.setLayout(gb);
     panel.setLayout(new GridLayout(6, 2));
     // Initialize the textfields
-    final JTextField barCodeField = new JTextField();
-    final JTextField quantityField = new JTextField();
-    final JTextField priceField = new JTextField();
-    final JTextField nameField = new JTextField();
-    final JTextField description = new JTextField();
+    barCodeField = new JTextField();
+    quantityField = new JTextField();
+    priceField = new JTextField();
+    nameField = new JTextField();
+    description = new JTextField();
 
     
     // - bar code
@@ -113,29 +134,26 @@ public class StockTab {
     gc.anchor = GridBagConstraints.NORTHWEST;
     gc.weightx = 0;
     
-    //TODO Järgnev kood tuleks kuidagi ümber muuta või õigesse kohta vahele lükata
-    /*addItem.addActionListener(new ActionListener() {
-   	 
-        public void actionPerformed(ActionEvent e)
-        {
-            StockItem addedItem = new StockItem(Long.parseLong(barCodeField.getText()),
-            		nameField.getText(),
-            		description.getText(),
-            		Double.parseDouble(quantityField.getText()),
-            		Integer.getInteger(priceField.getText()));
-            System.out.println(addedItem.getName());
-        }
-    });*/
 
-    addItem = new JButton("Add");
+    //addItem = new JButton("Add");
     gc.gridwidth = GridBagConstraints.RELATIVE;
     gc.weightx = 1.0;
+    addItem = createAddButton();
     panel.add(addItem, gc);
 
     panel.setBorder(BorderFactory.createLineBorder(Color.BLACK));
+
     return panel;
   }
 
+	public void addNewItem() {
+		StockItem addedItem = new StockItem(Long.parseLong(barCodeField.getText()),
+				nameField.getText(),description.getText(),
+				Double.parseDouble(priceField.getText()),
+				Integer.parseInt(quantityField.getText()));
+		model.getWarehouseTableModel().addItem(addedItem);
+		fireTableDataChanged();
+	}
 
   // table of the wareshouse stock
   private Component drawStockMainPane() {
